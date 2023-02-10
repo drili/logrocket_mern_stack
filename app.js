70ecd96de25df84f5ce57005a80b4a5258cd29ca
drili <DriliB@hotmail.com>
2023-02-10 13:32:53 +0100

    :hammer: Updated model and app.js [server]

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express")
 const connectDB = require("./config/db")
 
 const app = express()
+
+const books = require('./routes/api/books');
+
 connectDB()
 
 app.get("/", (req, res) => res.send("Hello world!"))
+
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8082
 
